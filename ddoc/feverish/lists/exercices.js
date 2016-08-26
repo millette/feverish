faa273df63ccfd4ffc874ed1b7a336c1f42eec87
Robin Millette <robin@millette.info>
2016-08-26 10:25:35 -0400

    Exercices list and template.

---
+++ ddoc/feverish/lists/exercices.js
@@ -0,0 +1,13 @@
+/* globals start, getRow, send */
+'use strict'
+module.exports = function (head, req, mocks) {
+  const tpl = require('views/lib/templates')
+  if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
+  start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
+
+  let row
+  const rows = []
+  while ((row = getRow())) { rows.push(row.doc) }
+
+  send(tpl.exercices({ rows: rows }))
+}
