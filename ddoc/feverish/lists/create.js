634e60bd7827be2594114f5c6182570a73212539
Robin Millette <robin@millette.info>
2016-08-26 06:35:02 -0400

    List themes (by popularity or alphabetically).

---
+++ ddoc/feverish/lists/create.js
@@ -0,0 +1,27 @@
+/* globals start, getRow, send */
+'use strict'
+module.exports = function (head, req, mocks) {
+  const tpl = require('views/lib/templates')
+  if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
+  const sortedRows = (order) => {
+    const themeSorter = (a, b) => {
+      if (order !== 'alpha') {
+        // first, sort by value in descending order
+        if (a.value > b.value) { return -1 }
+        if (a.value < b.value) { return 1 }
+      }
+      // next, sort by key alphabetically in ascending order
+      if (a.key > b.key) { return 1 }
+      // no duplicates, keys will always be different
+      return -1
+    }
+    let row
+    const rows = []
+    while ((row = getRow())) { rows.push(row) }
+    return rows.sort(themeSorter)
+  }
+  const rows = sortedRows(req.query.order)
+  start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
+  send(tpl.hi())
+  send('<pre>' + JSON.stringify(rows, null, ' ') + '</pre>')
+}
