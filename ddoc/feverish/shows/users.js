6948bf94960646d86f10ed74c75a201f4017fe0a
Robin Millette <robin@millette.info>
2016-08-27 06:30:27 -0400

    Show users.

---
+++ ddoc/feverish/shows/users.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = function (doc, req) {
+  if (doc) { return 'bad path' }
+  if (req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
+
+  const tpl = require('views/lib/templates')
+  return tpl.users(req.userCtx)
+}
