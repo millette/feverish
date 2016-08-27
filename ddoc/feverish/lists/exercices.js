51ebc6aee49825178cf610125de6aeab144e9df3
Robin Millette <robin@millette.info>
2016-08-27 10:17:48 -0400

    Touchups.

--- ddoc/feverish/lists/exercices.js
+++ ddoc/feverish/lists/exercices.js
@@ -4,10 +4,8 @@ module.exports = function (head, req, mocks) {
   const tpl = require('views/lib/templates')
   if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
   start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
-
   let row
   const rows = []
   while ((row = getRow())) { rows.push(row.doc) }
-
   send(tpl.exercices({ rows: rows }))
 }
--- ddoc/feverish/shows/users.js
+++ ddoc/feverish/shows/users.js
@@ -2,7 +2,6 @@
 module.exports = function (doc, req) {
   if (doc) { return 'bad path' }
   if (req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
-
   const tpl = require('views/lib/templates')
   return tpl.users(req.userCtx)
 }
