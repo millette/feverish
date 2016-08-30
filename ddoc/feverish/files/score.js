46b3d7272858b404f98edae8f85862d7a7dc1ecc
Robin Millette <robin@millette.info>
2016-08-30 06:53:26 -0400

    Replace multiple times in a row (bugfix).

--- ddoc/feverish/files/score.js
+++ ddoc/feverish/files/score.js
@@ -5,24 +5,28 @@ $(function () {
   const bodyData = $body.data()
   const $score = $('#json-score')
   const $accordion = $('#json-score + ul.accordion').hide()
+  var lastRev
 
-  const submitJpeg = function (bodyData, self, rev, ev) {
+  const submitJpeg = function (bodyData, self, ev) {
     // see http://stackoverflow.com/a/11910333/1154755
     const $self = $(self)
     const exid = bodyData.exercice
     const docUrl = $self.parents('form').attr('action')
     const file = $self[0].files[0]
+    const file2 = $self[0].files[0]
     const putRequest = new window.XMLHttpRequest()
     const fileReader = new window.FileReader()
     const fileReader2 = new window.FileReader()
-    putRequest.open('PUT', docUrl + '/' + exid + '.jpg' + '?rev=' + rev, true)
+    putRequest.open('PUT', docUrl + '/' + exid + '.jpg' + '?rev=' + lastRev, true)
     putRequest.setRequestHeader('Content-Type', file.type)
-    fileReader2.readAsDataURL(file)
+    fileReader2.readAsDataURL(file2)
     fileReader.readAsArrayBuffer(file)
     fileReader2.onload = function (readerEvent) { $('#reference-image img')[0].src = fileReader2.result }
     fileReader.onload = function (readerEvent) { putRequest.send(readerEvent.target.result) }
     putRequest.onreadystatechange = function (response) {
       if (putRequest.readyState === 4 && putRequest.status === 201) {
+        const etag = putRequest.getResponseHeader('etag')
+        lastRev = etag.slice(1, -1)
         $('label[for="fichier-label"]').addClass('success').text('Merci!')
         $accordion.foundation('toggle', $('#reference-image .accordion-content'))
       } else {
@@ -40,6 +44,7 @@ $(function () {
 
   const showScore = function (bodyData, userDoc, $score, score) {
     $accordion.show()
+    lastRev = userDoc._rev
     score.percent = Math.round(1000 * score.note / score.ponderation) / 10
     $score.addClass('success').html(makeHtml(score))
 
@@ -47,7 +52,7 @@ $(function () {
       $('#reference-image img').attr('src', ['/_users', userDoc._id, bodyData.exercice + '.jpg'].join('/'))
       $accordion.foundation('toggle', $('#reference-image .accordion-content'))
     }
-    $body.on('change', 'input#fichier-label', submitJpeg.bind(null, bodyData, 'input#fichier-label', userDoc._rev))
+    $body.on('change', 'input#fichier-label', submitJpeg.bind(null, bodyData, 'input#fichier-label'))
   }
 
   $.getJSON('/_users/org.couchdb.user:' + bodyData.student, function (userDoc) {
