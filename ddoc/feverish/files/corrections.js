a3492dbcae6d2db94fadb9ab914bd64c43f6800a
Robin Millette <robin@millette.info>
2016-08-29 23:13:19 -0400

    move client js to own file.

---
+++ ddoc/feverish/files/corrections.js
@@ -0,0 +1,34 @@
+/* globals $ */
+$(function () {
+  'use strict'
+  const exid = $('body').data('exid')
+  const getUsers = function () {
+    const fn = function (data) {
+      const filt = function (doc) {
+        return doc.roles.indexOf('student') !== -1 && (!doc.corrections || !doc.corrections[exid])
+      }
+      const docs = data.rows.map(function (row) { return row.doc })
+      const withAtts = docs.filter(filt)
+      const atts = withAtts.length
+        ? withAtts.map(function (doc) { return '<li><a href="/corrections/' + exid + '/' + doc.name + '">' + doc.name + '</a></li>' })
+        : ['<li>[PERSONNE]</li>']
+      $('ul#json').html(atts.join(''))
+
+      const filt2 = function (doc) {
+        return doc.roles.indexOf('student') !== -1 && doc.corrections && doc.corrections[exid]
+      }
+      const withAtts2 = docs.filter(filt2)
+      const atts2 = withAtts2.length
+        ? withAtts2.map(function (doc) { return '<li><a href="/corrections/' + exid + '/' + doc.name + '">' + doc.name + '</a></li>' })
+        : ['<li>[PERSONNE]</li>']
+      $('ul#json2').html(atts2.join(''))
+    }
+    const query = {
+      startkey: '"org.couchdb.user:"',
+      endkey: '"org.couchdb.user:\ufff0"',
+      include_docs: true
+    }
+    $.getJSON('/_users/_all_docs', query, fn)
+  }
+  getUsers()
+})
