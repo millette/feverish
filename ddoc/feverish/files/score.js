150a72b239c06f27bf95c0436fbbaf24db88b6ae
Robin Millette <robin@millette.info>
2016-08-29 23:26:29 -0400

    Satisfy linter.

--- ddoc/feverish/files/score.js
+++ ddoc/feverish/files/score.js
@@ -1,6 +1,7 @@
 /* globals $ */
 $(function () {
   'use strict'
+/*
   const score = function ($form, ponderation) {
     const ret = { ponderation: ponderation }
     $form.serializeArray().forEach(function (x) { ret[x.name] = x.value })
@@ -9,19 +10,20 @@ $(function () {
     ret.createdAt = new Date().toISOString()
     return ret
   }
+*/
 
   const $body = $('body')
   const bodyData = $body.data()
   const $score = $('#json-score')
 
-  const submitJpeg =  function (bodyData, self, rev, ev) {
+  const submitJpeg = function (bodyData, self, rev, ev) {
     // see http://stackoverflow.com/a/11910333/1154755
     const $self = $(self)
     const exid = bodyData.exercice
     const docUrl = $self.parents('form').attr('action')
     const file = $self[0].files[0]
-    const putRequest = new XMLHttpRequest()
-    const fileReader = new FileReader()
+    const putRequest = new window.XMLHttpRequest()
+    const fileReader = new window.FileReader()
     putRequest.open('PUT', docUrl + '/' + exid + '.jpg' + '?rev=' + rev, true)
     putRequest.setRequestHeader('Content-Type', file.type)
     fileReader.readAsArrayBuffer(file)
@@ -38,7 +40,7 @@ $(function () {
 
   const showUpload = function (student, $score) {
     $score.after([
-      '<form id="upload-jpeg" action="/_users/org.couchdb.user:'+ student + '">',
+      '<form id="upload-jpeg" action="/_users/org.couchdb.user:' + student + '">',
       '  <fieldset class="fieldset callout primary">',
       '  <h1 class="title text-center">Téléverser jpeg</h1>',
       '  <div class="row">',
@@ -56,14 +58,14 @@ $(function () {
     $score.after('<img src="/_users/' + userDoc._id + '/' + exercice + '.jpg' + '" alt="jpeg">')
   }
 
-  const makeHtml = function (score) {
-    return ('<p><span class="stat">' + score.percent + '%' + '</span> ' + score.note + '/' + score.ponderation + '</p>'
-     + '<p><i>' + score.createdAt.split('T')[0] + '</i> ' + score.commentaires + '</p>')
+  const makeHtml = function (score2) {
+    return ('<p><span class="stat">' + score2.percent + '%' + '</span> ' + score2.note + '/' + score2.ponderation + '</p>' +
+     '<p><i>' + score2.createdAt.split('T')[0] + '</i> ' + score2.commentaires + '</p>')
   }
 
-  const showScore = function (bodyData, userDoc, $score, score) {
-    score.percent = Math.round(1000 * score.note / score.ponderation) / 10
-    $score.addClass('success').html(makeHtml(score))
+  const showScore = function (bodyData, userDoc, $score, score3) {
+    score3.percent = Math.round(1000 * score3.note / score3.ponderation) / 10
+    $score.addClass('success').html(makeHtml(score3))
     if (userDoc._attachments && userDoc._attachments[bodyData.exercice + '.jpg']) {
       showJpeg(bodyData.exercice, userDoc)
     } else {
@@ -73,9 +75,9 @@ $(function () {
   }
 
   $.getJSON('/_users/org.couchdb.user:' + bodyData.student, function (userDoc) {
-    const score = userDoc.corrections && userDoc.corrections[bodyData.exercice]
-    if (score) {
-      showScore(bodyData, userDoc, $score, score)
+    const score4 = userDoc.corrections && userDoc.corrections[bodyData.exercice]
+    if (score4) {
+      showScore(bodyData, userDoc, $score, score4)
     } else {
       $score.addClass('warning').text('Aucun résultat disponible.')
     }
