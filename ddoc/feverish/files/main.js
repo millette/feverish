bef55c0ac3189199e5ce750cbca710596c4b9177
Robin Millette <robin@millette.info>
2016-08-29 23:05:14 -0400

    touchups.

--- ddoc/feverish/files/main.js
+++ ddoc/feverish/files/main.js
@@ -2,16 +2,11 @@
 $(function () {
   'use strict'
   $(document).foundation()
-
   $('.top-bar > .top-bar-right > .menu > li > button.button').click(function (ev) {
-    // console.log('want to logout')
-    // console.log('EV:', ev)
     $.ajax({
       url: '/_session',
       method: 'DELETE',
-      complete: function () {
-        window.location = '/_utils/groupe2016.html'
-      }
+      complete: function () { window.location = '/_utils/groupe2016.html' }
     })
   })
 })
