f9ab4660aa327719cc4b07b120ed39de5dc53201
Robin Millette <robin@millette.info>
2016-08-29 23:01:00 -0400

    bugfix: only set createdAt on creation.

--- ddoc/feverish/updates/create.js
+++ ddoc/feverish/updates/create.js
@@ -2,7 +2,6 @@
 module.exports = function (doc, req) {
   var r
   req.form.creator = req.userCtx.name
-  req.form.createdAt = new Date().toISOString()
   req.form.ponderation = parseFloat(req.form.ponderation)
   req.form.descriptif = req.form.descriptif.trim()
   req.form.theme = req.form.theme.trim()
@@ -12,7 +11,7 @@ module.exports = function (doc, req) {
     for (r in req.form) { doc[r] = req.form[r] }
     return [doc, JSON.stringify(doc, null, ' ')]
   }
-
+  req.form.createdAt = new Date().toISOString()
   req.form._id = req.uuid
   return [req.form, JSON.stringify(req.form, null, ' ')]
 }
