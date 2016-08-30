4094f489df64e3d6873b3411092f8895df18201f
Robin Millette <robin@millette.info>
2016-08-29 22:26:20 -0400

    Main menu on exercice creation/edition.

--- ddoc/feverish/lists/create.js
+++ ddoc/feverish/lists/create.js
@@ -27,6 +27,7 @@ module.exports = function (head, req, mocks) {
   }
   const rows = sortedRows(req.query.order)
   start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
-  const themeOption = (row) => `<option value="${row.key}">${row.key} (${row.value})</option>`
-  send(tpl['create-exercice']({ themeOption: themeOption, rows: rows }))
+  req.userCtx.themeOption = (row) => `<option value="${row.key}">${row.key} (${row.value})</option>`
+  req.userCtx.rows = rows
+  send(tpl['create-exercice'](req.userCtx))
 }
