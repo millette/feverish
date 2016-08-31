6e44e9b5dc8a5898d3f7a27b84d7da90954ec6cb
Robin Millette <robin@millette.info>
2016-08-31 07:44:36 -0400

    Pass title and content to welcome template.

--- ddoc/feverish/shows/bienvenue.js
+++ ddoc/feverish/shows/bienvenue.js
@@ -1,6 +1,12 @@
 'use strict'
 module.exports = function (doc, req) {
-  if (doc) { return 'bad path' }
+  if (!doc || doc._id !== 'accueil') { return 'bad path' }
   const tpl = require('views/lib/templates')
+  req.userCtx.editor =
+    req.userCtx.roles.indexOf('teacher') !== -1 ||
+    req.userCtx.roles.indexOf('_admin') !== -1
+
+  req.userCtx.content = doc.content
+  req.userCtx.title = doc.title
   return tpl.bienvenue(req.userCtx)
 }
