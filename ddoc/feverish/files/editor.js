eb0cf1f8e348c29be03b6b4f2c06568753552699
Robin Millette <robin@millette.info>
2016-08-31 07:45:24 -0400

    Client side editor handler.

---
+++ ddoc/feverish/files/editor.js
@@ -0,0 +1,53 @@
+/* globals $, MediumEditor */
+$(function () {
+  'use strict'
+
+  var dirty = false
+
+  var $saver = $('h1.title > button')
+
+  const show = function (maybe) {
+    if (maybe) {
+      $('#main-editor *').removeClass('lead')
+      $('#main-editor > p').first().addClass('lead')
+      if (!dirty) { return }
+    } else {
+      $('h1.title > span').text($('#main-editor > h1').text())
+    }
+    dirty = true
+    $saver.show()
+  }
+
+  $(window).on('beforeunload', function () {
+    if (dirty) { return 'Attention, vous n\'avez pas sauvegardé la page. Voulez-vous vraiment quitter cette page?' }
+  })
+
+  $('#main-editor > p:first-child').addClass('lead')
+  const $ed = $('#main-editor')
+  const x = new MediumEditor($ed[0], {
+    autoLink: true,
+    placeholder: { text: 'Tapez votre texte ici.' },
+    toolbar: { buttons: ['h1', 'h2', 'h3', 'bold', 'italic', 'orderedlist', 'unorderedlist', 'quote'] }
+  })
+
+  x.subscribe('blur', show.bind(null, true))
+  x.subscribe('editableInput', show.bind(null, false))
+  $saver.hide()
+
+  $saver.click(function (ev) {
+    ev.preventDefault()
+    const yeah = function (a, b, c, d) {
+      if (a !== 'ok') { return console.log('saved error', a, b, c, d) }
+      dirty = false
+      $saver.hide()
+      $('#main-editor > h1').remove()
+    }
+
+    $.ajax({
+      method: 'POST',
+      data: $('#main-editor').html(),
+      success: yeah,
+      error: function (a, b, c, d) { console.log('oups saving', a, b, c, d) }
+    })
+  })
+})
