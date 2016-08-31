15e7700947203f40ffe1de54f33c4ba3ab902626
Robin Millette <robin@millette.info>
2016-08-31 09:47:27 -0400

    Relax validate_doc_update() a bit.

--- ddoc/feverish/validate_doc_update.js
+++ ddoc/feverish/validate_doc_update.js
@@ -3,6 +3,7 @@ module.exports = function (newDoc, oldDoc, userCtx, secObj) {
   const ouch = (err) => { throw err }
   const adminDelete = newDoc._deleted && userCtx.roles.indexOf('_admin') !== -1
   if (userCtx.roles.indexOf('teacher') === -1 && userCtx.roles.indexOf('_admin') === -1) { ouch({ unauthorized: 'not a teacher' }) }
+  if (newDoc._id === 'autocompleter' && !newDoc._deleted) { return }
   if (!adminDelete && userCtx.name !== newDoc.creator && !newDoc.content) { ouch({ forbidden: 'wrong creator' }) }
   if (!adminDelete && !newDoc.createdAt) { ouch({ forbidden: 'missing field: createdAt' }) }
   if (!adminDelete && oldDoc) {
