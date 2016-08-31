0000f05ca62de4195258c5be05fc275e59abe271
Robin Millette <robin@millette.info>
2016-08-31 02:08:11 -0400

    Allow admin to delete stuff.

--- ddoc/feverish/validate_doc_update.js
+++ ddoc/feverish/validate_doc_update.js
@@ -1,10 +1,12 @@
 'use strict'
 module.exports = function (newDoc, oldDoc, userCtx, secObj) {
   const ouch = (err) => { throw err }
+
+  const adminDelete = newDoc._deleted && userCtx.roles.indexOf('_admin') -== -1
   if (userCtx.roles.indexOf('teacher') === -1 && userCtx.roles.indexOf('_admin') === -1) { ouch({ unauthorized: 'not a teacher' }) }
-  if (userCtx.name !== newDoc.creator) { ouch({ forbidden: 'wrong creator' }) }
-  if (!newDoc.createdAt) { ouch({ forbidden: 'missing field: createdAt' }) }
-  if (oldDoc) {
+  if (!adminDelete && userCtx.name !== newDoc.creator) { ouch({ forbidden: 'wrong creator' }) }
+  if (!adminDelete && !newDoc.createdAt) { ouch({ forbidden: 'missing field: createdAt' }) }
+  if (!adminDelete && oldDoc) {
     if (!newDoc.updatedAt) { ouch({ forbidden: 'missing field: updatedAt' }) }
     if (oldDoc.updatedAt >= newDoc.updatedAt) { ouch({ forbidden: 'field updatedAt went back in time' }) }
   }
