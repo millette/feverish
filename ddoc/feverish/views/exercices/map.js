a7e8f77917ebf709b67b1c38ecc5bd11f4683903
Robin Millette <robin@millette.info>
2016-08-31 02:25:13 -0400

    Don't show autocompleter doc as exercice.

--- ddoc/feverish/views/exercices/map.js
+++ ddoc/feverish/views/exercices/map.js
@@ -1,7 +1,7 @@
 /* globals emit */
 'use strict'
 module.exports = function (doc, mocks) {
-  if (!doc.theme || !doc.creator || !doc.createdAt) { return }
+  if (doc._id === 'autocompleter' || !doc.theme || !doc.creator || !doc.createdAt) { return }
   if (!mocks) { mocks = { emit: emit } }
   mocks.emit(doc.createdAt)
 }
