3fe11635be77efaedc107d53bad90e6cc57d1f37
Robin Millette <robin@millette.info>
2016-08-31 01:30:11 -0400

    Don't use list function to create exercice.

--- ddoc/feverish/shows/edit.js
+++ ddoc/feverish/shows/edit.js
@@ -2,7 +2,7 @@
 module.exports = function (doc, req) {
   if (req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
   const tpl = require('views/lib/templates')
-  if (doc.description && !doc.descriptif) { doc.descriptif = doc.description }
+  if (doc && doc.description && !doc.descriptif) { doc.descriptif = doc.description }
   req.userCtx.doc = doc
   return tpl['create-exercice'](req.userCtx)
 }
