c02d90f4d4d5894e3d97f33d9d6a37eca0fad453
Robin Millette <robin@millette.info>
2016-08-31 01:25:30 -0400

    Remove select, always use autocomplete (themes).

--- ddoc/feverish/lists/create.js
+++ ddoc/feverish/lists/create.js
@@ -8,13 +8,11 @@ module.exports = function (head, req, mocks) {
   }
   const tpl = require('views/lib/templates')
   if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
-  const sortedRows = (order) => {
+  const sortedRows = () => {
     const themeSorter = (a, b) => {
-      if (order !== 'alpha') {
-        // first, sort by value in descending order
-        if (a.value > b.value) { return -1 }
-        if (a.value < b.value) { return 1 }
-      }
+      // first, sort by value in descending order
+      if (a.value > b.value) { return -1 }
+      if (a.value < b.value) { return 1 }
       // next, sort by key alphabetically in ascending order
       if (a.key > b.key) { return 1 }
       // no duplicates, keys will always be different
@@ -25,7 +23,7 @@ module.exports = function (head, req, mocks) {
     while ((row = getRow())) { rows.push(row) }
     return rows.sort(themeSorter)
   }
-  const rows = sortedRows(req.query.order)
+  const rows = sortedRows()
   start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
   req.userCtx.themeOption = (row) => `<option value="${row.key}">${row.key} (${row.value})</option>`
   req.userCtx.rows = rows
