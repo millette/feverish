c5a9778d80d8b74ecda1adf888dcda76263d1ed3
Robin Millette <robin@millette.info>
2016-09-01 04:50:57 -0400

    Admin can change his password.

--- ddoc/feverish/files/main.js
+++ ddoc/feverish/files/main.js
@@ -28,22 +28,37 @@ $(function () {
   $('body').on('submit', '#changePassword > form', function (ev) {
     ev.preventDefault()
     const $form = $(this)
-    const pwds = $form.serializeArray()
-      .map(function (i) { return i.value })
-      .slice(0, 2)
-      .filter(function (i) { return i })
-
-    if (pwds.length !== 2 || pwds[0] !== pwds[1]) {
+    const adminname = $('button.adminname').data('adminname')
+    const badAttempt = function () {
       $form.addClass('shake-horizontal shake-constant')
       setTimeout(function () {
         $form.removeClass('shake-horizontal shake-constant')
       }, 500)
-      return
     }
-    console.log('Ready to change password!')
-    // get user doc
-    // set password field
-    // put user doc
+    const pwds = $form.serializeArray()
+      .map(function (i) { return i.value })
+      .slice(0, 2)
+      .filter(function (i) { return i })
+    if (pwds.length !== 2 || pwds[0] !== pwds[1]) { return badAttempt() }
+    $.ajax({
+      dataType: 'json',
+      url: '/_users/org.couchdb.user:' + adminname,
+      success: function (userDoc, b, c, d) {
+        if (userDoc.roles.indexOf('teacher') === -1 && userDoc.roles.indexOf('_admin') === -1) { return badAttempt() }
+        $.ajax({
+          dataType: 'json',
+          method: 'PUT',
+          url: '/admin/' + adminname,
+          data: JSON.stringify(pwds[0]),
+          error: function (a, b, c, d) {
+            badAttempt()
+          },
+          success: function (a, b, c, d) {
+            $('#changePassword').foundation('close')
+          }
+        })
+      }
+    })
   })
 
   $('button.logout').click(function (ev) {
