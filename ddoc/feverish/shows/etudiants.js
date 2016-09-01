0012186d354b197383a62825b742b30360c9044f
Robin Millette <robin@millette.info>
2016-09-01 14:28:40 -0400

    Ensure /etudiants/:user and /resultats use the same template and logic.

--- ddoc/feverish/shows/etudiants.js
+++ ddoc/feverish/shows/etudiants.js
@@ -1,12 +1,16 @@
 'use strict'
 module.exports = function (doc, req) {
   if (doc) { return 'bad path' }
-  if (req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
+  if (!req.query.self && req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
   const tpl = require('views/lib/templates')
+  if (req.query.self) {
+    req.userCtx.self = true
+    req.userCtx.student = req.userCtx.name
+    return tpl.etudiant(req.userCtx)
+  }
   if (req.query.user) {
     req.userCtx.student = req.query.user
     return tpl.etudiant(req.userCtx)
-  } else {
-    return tpl.etudiants(req.userCtx)
   }
+  return tpl.etudiants(req.userCtx)
 }
