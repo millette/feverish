72d65aaaa19fdb09c47ffe51cde30922ce144501
Robin Millette <robin@millette.info>
2016-09-01 12:06:26 -0400

    Placeholder page for coming features.

---
+++ ddoc/feverish/shows/notyet.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = function (doc, req) {
+  if (doc) { return 'bad path' }
+  const tpl = require('views/lib/templates')
+  return tpl.notyet(req.userCtx)
+}
--- ddoc/feverish/shows/users.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-module.exports = function (doc, req) {
-  if (doc) { return 'bad path' }
-  if (req.userCtx.roles.indexOf('teacher') === -1 && req.userCtx.roles.indexOf('_admin') === -1) { return 'not a teacher' }
-  const tpl = require('views/lib/templates')
-  return tpl.users(req.userCtx)
-}
