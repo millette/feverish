c563bf61e48cfbc05f3d98da2882a273ca895e99
Robin Millette <robin@millette.info>
2016-09-02 12:57:21 -0400

    Improve exercices page a bit.

--- ddoc/feverish/lists/exercices.js
+++ ddoc/feverish/lists/exercices.js
@@ -3,12 +3,13 @@
 module.exports = function (head, req, mocks) {
   const tpl = require('views/lib/templates')
   const studentMenu = function (row) {
-    return '<li><a href="/score/' + row._id + '">Consulter mon résultat</a></li>'
+    return '<a class="button success" href="/score/' + row._id + '">Consulter mon résultat</a>'
   }
   const teacherMenu = function (row) {
     return [
-      '<li><a href="/edit/' + row._id + '">Éditer</a></li>',
-      '<li><a href="/corrections/' + row._id + '">Corriger</a></li>'
+      '<a class="button" href="/edit/' + row._id + '">Éditer</a>',
+      '<a class="button success" href="/corrections/' + row._id + '">Corriger</a>',
+      '<button type="button" class="button warning" data-exid="' + row._id + '">Effacer</button>'
     ].join('')
   }
 
