8180eaab58b7a2ab8227bdceb1f10bb723eae306
Robin Millette <robin@millette.info>
2016-09-03 13:46:08 -0400

    Steps toward student attachment to exercice docs.

--- ddoc/feverish/validate_doc_update.js
+++ ddoc/feverish/validate_doc_update.js
@@ -1,6 +1,53 @@
+/* globals log */
 'use strict'
-module.exports = function (newDoc, oldDoc, userCtx, secObj) {
+module.exports = function (newDoc, oldDoc, userCtx, secObj, mocks) {
+  if (!mocks) { mocks = { log: log } }
+  const sentinel = function (x) { return x.indexOf('}{') !== -1 }
+  const internals = ['_id', '_rev', '_revisions', '_attachments']
+  const internal = function (x) { return internals.indexOf(x) === -1 }
+  const kn = Object.keys(newDoc).filter(internal).sort()
   const ouch = (err) => { throw err }
+
+  mocks.log(userCtx)
+  mocks.log(kn)
+
+  const validateStudent = function () {
+    const currentAtt = []
+    var r
+    const ko = Object.keys(oldDoc).filter(internal).sort()
+    if (userCtx.roles.indexOf('student') !== -1) {
+      mocks.log(ko)
+
+      // docs must have the same fields
+      if (!ko.length || ko.length !== kn.length || ko.join('}{') !== kn.join('}{')) { ouch({ forbidden: 'student, can\'t touch this' }) }
+      if (!newDoc._attachments) { ouch({ forbidden: 'expecting a jpeg from student' }) }
+      for (r in newDoc._attachments) { if (newDoc._attachments[r].revpos === 0) { currentAtt.push(r) } }
+      if (currentAtt.length !== 1 || currentAtt[0].slice(-4) !== '.jpg') { ouch({ forbidden: 'expecting a (single) jpeg from student' }) }
+      /*
+       * At this point, we have what seems like a valid attachment upload
+       * We must test one last thing:
+       * Does the attachment name correspond to one of the accepted references?
+       * Perhaps we can (ab)use the roles and stick an ID in there...
+       *
+       * The attribute name must correspond to one the the user's roles
+       * but the att. name must NOT match the user's name (untraceable)
+       * NOR the exercice _id (we'll accept many atts for same exercice)
+       *
+       * Two ways to achieve this:
+       * 1. create each user with a unique opaque random id (field or even a role)
+       * 2. at the moment the teacher scores a student.....
+      */
+      if (userCtx.roles.indexOf(currentAtt[0].slice(0, -4)) === -1) { ouch({ forbidden: 'not expecting a jpeg from this student' }) }
+
+      ouch({ unauthorized: 'hey, a student' })
+      return
+    }
+  }
+
+  if (kn.filter(sentinel).length) { ouch({ forbidden: 'illegal characters in field name' }) }
+
+  validateStudent()
+
   const adminDelete = newDoc._deleted && userCtx.roles.indexOf('_admin') !== -1
   if (userCtx.roles.indexOf('teacher') === -1 && userCtx.roles.indexOf('_admin') === -1) { ouch({ unauthorized: 'not a teacher' }) }
   if (newDoc._id === 'autocompleter' && !newDoc._deleted) { return }
