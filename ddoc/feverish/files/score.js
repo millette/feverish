11ab3572c0afcb32266c895a4aa5666ea27e31f2
Robin Millette <robin@millette.info>
2016-09-03 08:15:35 -0400

    Refactor jpeg upload a bit.

--- ddoc/feverish/files/score.js
+++ ddoc/feverish/files/score.js
@@ -5,35 +5,49 @@ $(function () {
   const bodyData = $body.data()
   const $score = $('#json-score')
   const $accordion = $('#json-score + #reference-image').hide()
-  var lastRev
+  const lastRevs = { }
+
+  const putFile = function (docUrl, exid, file, forUser) {
+    // see http://stackoverflow.com/a/11910333/1154755
+    const putRequest = new window.XMLHttpRequest()
+    // TODO: PUT jpg to exercice doc too
+    putRequest.open('PUT', docUrl + '/' + exid + '.jpg' + '?rev=' + lastRevs[forUser ? 'userdoc' : 'exdoc'], true)
+    putRequest.setRequestHeader('Content-Type', file.type)
+    putRequest.onreadystatechange = function (response) {
+      if (putRequest.readyState !== 4) { return }
+      if (putRequest.status === 201) {
+        const etag = putRequest.getResponseHeader('etag')
+        lastRevs[forUser ? 'userdoc' : 'exdoc'] = etag.slice(1, -1)
+        if (forUser) {
+          $('label[for="fichier-label"]').addClass('success') // .text('Merci!')
+          $('#reference-image img').attr('title', 'Merci!')
+        }
+      } else {
+        if (forUser) {
+          $('label[for="fichier-label"]').addClass('warning') // .text('Erreur #1...')
+          $('#reference-image img').attr('title', 'Erreur #1...')
+        }
+      }
+    }
+    return putRequest
+  }
 
   const submitJpeg = function (bodyData, self, ev) {
-    // see http://stackoverflow.com/a/11910333/1154755
     const $self = $(self)
     const exid = bodyData.exercice
     const docUrl = $self.parents('form').attr('action')
     const file = $self[0].files[0]
     const file2 = $self[0].files[0]
-    const putRequest = new window.XMLHttpRequest()
+
+    const putRequest = putFile(docUrl, exid, file, true)
+    // TODO: PUT jpg to exercice doc too
+
     const fileReader = new window.FileReader()
-    const fileReader2 = new window.FileReader()
-    putRequest.open('PUT', docUrl + '/' + exid + '.jpg' + '?rev=' + lastRev, true)
-    putRequest.setRequestHeader('Content-Type', file.type)
-    fileReader2.readAsDataURL(file2)
-    fileReader.readAsArrayBuffer(file)
-    fileReader2.onload = function (readerEvent) { $('#reference-image img')[0].src = fileReader2.result }
     fileReader.onload = function (readerEvent) { putRequest.send(readerEvent.target.result) }
-    putRequest.onreadystatechange = function (response) {
-      if (putRequest.readyState === 4 && putRequest.status === 201) {
-        const etag = putRequest.getResponseHeader('etag')
-        lastRev = etag.slice(1, -1)
-        $('label[for="fichier-label"]').addClass('success') // .text('Merci!')
-        $('#reference-image img').attr('title', 'Merci!')
-      } else {
-        $('label[for="fichier-label"]').addClass('warning') // .text('Erreur #1...')
-        $('#reference-image img').attr('title', 'Erreur #1...')
-      }
-    }
+    fileReader.readAsArrayBuffer(file)
+    const fileReader2 = new window.FileReader()
+    fileReader2.onload = function (readerEvent) { $('#reference-image img')[0].src = fileReader2.result }
+    fileReader2.readAsDataURL(file2)
   }
 
   const makeHtml = function (score) {
@@ -45,7 +59,7 @@ $(function () {
 
   const showScore = function (bodyData, userDoc, $score, score) {
     $accordion.show()
-    lastRev = userDoc._rev
+    lastRevs.userdoc = userDoc._rev
     score.percent = Math.round(1000 * score.note / score.ponderation) / 10
     $score.addClass('success').html(makeHtml(score))
     if (userDoc._attachments && userDoc._attachments[bodyData.exercice + '.jpg']) {
