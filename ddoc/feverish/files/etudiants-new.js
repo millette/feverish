0ccf49411985a9485013032a162dfb7842db55a6
Robin Millette <robin@millette.info>
2016-09-04 06:53:08 -0400

    Add opaque id to users on creation.

--- ddoc/feverish/files/etudiants-new.js
+++ ddoc/feverish/files/etudiants-new.js
@@ -17,23 +17,26 @@ $(function () {
     if (pwds.length !== 3 || pwds[2] !== pwds[1]) { return badAttempt() }
     const parts = pwds[0].split(' ')
     if (parts.length !== 2) { return badAttempt() }
-    const uid = 'org.couchdb.user:' + pwds[0]
-    const userDoc = {
-      _id: uid,
-      name: pwds[0],
-      type: 'user',
-      roles: ['student'],
-      password: pwds[1]
-    }
-    $.ajax({
-      url: '/_users/' + uid,
-      dataType: 'json',
-      data: JSON.stringify(userDoc),
-      method: 'PUT',
-      error: function () { badAttempt() },
-      success: function (userDoc) {
-        window.location = '/etudiants'
+    $.getJSON('/_uuids', function (uuids) {
+      const uid = 'org.couchdb.user:' + pwds[0]
+      const userDoc = {
+        _id: uid,
+        name: pwds[0],
+        opaque: uuids.uuids[0],
+        type: 'user',
+        roles: ['student'],
+        password: pwds[1]
       }
+      $.ajax({
+        url: '/_users/' + uid,
+        dataType: 'json',
+        data: JSON.stringify(userDoc),
+        method: 'PUT',
+        error: function () { badAttempt() },
+        success: function (userDoc) {
+          window.location = '/etudiants'
+        }
+      })
     })
   })
 })
