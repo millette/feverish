03c6fdc1091cbac69da3cbc4318547da4b4ae228
Robin Millette <robin@millette.info>
2016-09-04 17:40:19 -0400

    Fix autocompleter js.

--- ddoc/feverish/files/autocompleter.js
+++ ddoc/feverish/files/autocompleter.js
@@ -1,14 +1,18 @@
 /* globals $ */
 $(function () {
   'use strict'
-  const $input = $('input[type="file"]')
+  const fn = function ($who, a, b, c, d) {
+    const $sub = $('input[type="submit"]', $who)
+    $sub.val('Merci!')
+    $sub.removeClass('secondary')
+    $sub.addClass('success')
+  }
   $('#example-vert-tabs').on('change.zf.tabs', function (ev) {
     const $sub = $('.tabs-content input[type="submit"]')
     $sub.addClass('secondary')
     $sub.removeClass('success')
     $sub.val('Soumettre')
   })
-
   $('form.configure').submit(function (ev) {
     const $form = $(this)
     const data = $form.serialize()
