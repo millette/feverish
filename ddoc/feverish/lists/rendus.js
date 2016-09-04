8aebd758bcb2ae96dc30ad392feb7a9b8d520937
Robin Millette <robin@millette.info>
2016-09-04 16:44:00 -0400

    Choice of rendus view.

--- ddoc/feverish/lists/rendus.js
+++ ddoc/feverish/lists/rendus.js
@@ -2,13 +2,11 @@
 'use strict'
 module.exports = function (head, req, mocks) {
   const tpl = require('views/lib/templates')
-
   if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
   start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
   let row
   const rows = []
   while ((row = getRow())) { rows.push(row.doc) }
-
   req.userCtx.rows = rows
-  send(tpl.rendus(req.userCtx))
+  send(tpl[req.query.v2 || 'rendus'](req.userCtx))
 }
