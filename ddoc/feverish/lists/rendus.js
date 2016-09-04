1eb364d5029f9a48ac1ae6629cf24bf647a5fade
Robin Millette <robin@millette.info>
2016-09-04 13:51:53 -0400

    Show reference images.

---
+++ ddoc/feverish/lists/rendus.js
@@ -0,0 +1,14 @@
+/* globals start, getRow, send */
+'use strict'
+module.exports = function (head, req, mocks) {
+  const tpl = require('views/lib/templates')
+
+  if (!mocks) { mocks = { start: start, getRow: getRow, send: send } }
+  start({ headers: { 'Content-Type': 'text/html; charset=utf-8' } })
+  let row
+  const rows = []
+  while ((row = getRow())) { rows.push(row.doc) }
+
+  req.userCtx.rows = rows
+  send(tpl.rendus(req.userCtx))
+}
